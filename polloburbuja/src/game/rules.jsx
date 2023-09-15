10211ee0605788275b16858d46bf9439b7be85aa
ianfryastorga <ianfryastorga@gmail.com>
2023-09-15 17:28:29 -0300

    carrousel rules

--- polloburbuja/src/game/rules.jsx
+++ polloburbuja/src/game/rules.jsx
@@ -1,9 +1,23 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 import Layout from '../layout.jsx'
 
 function Rules() {
   return (
     <Layout>
-      <h1>Aquí las reglasssss</h1>
+      <Carousel showThumbs={false} showStatus={false}>
+        <div>
+          <p>HOLA MUNDO</p>
+          <img className="carousel-image" src="src\assets\img\among_us.png" />
+        </div>
+        <div>
+          <img className="carousel-image" src="src\assets\img\baby_yoda.png" />
+        </div>
+        <div>
+          <img className="carousel-image" src="src\assets\img\mickey_mouse.png" />
+        </div>
+      </Carousel>
     </Layout>
   )
 }
