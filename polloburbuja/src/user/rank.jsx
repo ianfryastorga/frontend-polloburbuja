4d3858d61d405697099ecbb8f102255972dfd97e
CRISTÓBAL TOLORZA <christ.tolorza@uc.cl>
2023-09-19 01:59:13 -0300

    avance rank

--- polloburbuja/src/user/rank.jsx
+++ polloburbuja/src/user/rank.jsx
@@ -3,7 +3,25 @@ import Layout from '../layout.jsx'
 function Rank() {
   return (
     <Layout>
-      <h1>Aquí el ranking</h1>
+      <div class="container-rank">
+        <h2> Ranking Mejores Jugadores</h2>
+        <ol class="lista-rank">
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+          <li>Pedro 38 victorias</li>
+        </ol>
+
+        <ol start="42" class="lista-rank">
+          <li> Usuario 2 victorias</li>
+        </ol>
+      </div>
     </Layout>
   )
 }
