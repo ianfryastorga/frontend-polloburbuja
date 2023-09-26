65c1ee70ad657a20ac9ea2818926fd4177b9e9fe
luisramirezzg <luisramirezzg@uc.cl>
2023-09-26 15:02:40 -0300

    regis

--- polloburbuja/src/user/register/register.jsx
+++ polloburbuja/src/user/register/register.jsx
@@ -3,7 +3,27 @@ import Layout from '../../layout.jsx'
 function Register() {
   return (
     <Layout>
-      <h1>Aquí register</h1>
+      <div className='register_container'>
+        <h3>Ingrese su nombre:</h3>
+        <input type='text' placeholder="Nombre" className='input_register_nombre'></input>
+
+        <h3>Ingrese su apellido:</h3>
+        <input type="password" placeholder='Apellido' className='input_register_apellido'></input>
+        
+        <h3>Ingrese su correo electrónico:</h3>
+        <input type='text' placeholder='Correo Electrónico' className='input_register_correo'></input>
+
+        <h3>Ingrese su contraseña:</h3>
+        <input type="password" placeholder='Contraseña' className='input_register_contraseña'></input>
+        
+        <label for="miCheckpoint">Acepto los terminos y condiciones:</label>
+        <input type="checkbox" id="miCheckpoint" name="miCheckpoint" value="check"></input>
+        
+        <button type = "submit" className='boton_register'>
+          Registrarse
+        </button>
+
+      </div>
     </Layout>
   )
 }
