008891192895248808c38ff45c6d8c3cfe2e74fc
CRISTÓBAL TOLORZA <christ.tolorza@uc.cl>
2023-09-26 22:12:13 -0300

    about us ready

--- polloburbuja/src/game/about_us/aboutus.jsx
+++ polloburbuja/src/game/about_us/aboutus.jsx
@@ -5,23 +5,23 @@ function AboutUs() {
   return (
     <Layout>
       <div class="container-aboutus">
-        <h2>Sobre Nosotros</h2>
-        <h3>Grupo PolloBurbuja</h3>
+        <h1>Sobre Nosotros</h1>
+        <h3>Grupo: PolloBurbuja</h3>
         <div class="aboutus-grid">
             <div class="container-grids">
               <h3>Ian Fry</h3>
-              <p> ianfryastorga@uc.cl </p>
-              <p> ianfryastorga </p>
+              <p> - ianfryastorga@uc.cl </p>
+              <p> - ianfryastorga </p>
             </div>
             <div class="container-grids">
-              <h3>Luis Ramirez</h3>
-              <p> Luisramirezzg@uc.cl </p>
-              <p> Luisramirezzg </p>
+              <h3> Luis Ramirez</h3>
+              <p> - Luisramirezzg@uc.cl </p>
+              <p> - Luisramirezzg </p>
             </div>
             <div class="container-grids">
               <h3>Cristóbal Tolorza</h3>
-              <p> Christ.tolorza@uc.cl </p>
-              <p> Christ711 </p>
+              <p> - Christ.tolorza@uc.cl </p>
+              <p> - Christ711 </p>
             </div>
         </div>
       </div>
